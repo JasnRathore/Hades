1a815d7d05b2e93d8df92732e7701a412f2c27e1
KeanDsouza <146367498+KeanDsouza@users.noreply.github.com>
2023-09-28 21:38:16 +0530

    HADES.py
    Updated Login

--- HADES.py
+++ HADES.py
@@ -1,13 +1,62 @@
 from Initialisation import *
 from HORUS import *
 from CRONUS import *
+import random
 
 def main():
     Cursor, DataBase, Connected = init()
     if Connected is True:
-        #we can do menu(Cursor, DataBase) or menu(DataBase) and we define the cursor in the funtion
-        menu(Cursor, "guards")
+        Cursor.execute("SELECT * FROM Login;")
+        #User and Password details
+        Data = Cursor.fetchall()
+        Users,Passw=[],[] 
+        for rec in Data:
+            Users.append(rec[0])
+            Passw.append(rec[1])
+        while True:
+            print("""\n-----------------------------HADES LOGIN-----------------------------\n
+1) Login
+2) Change Password
+3) Exit\n""")
+            chc = int(input("Select a choice [1/2/3]: "))
+            #Entering User
+            if chc == 1:
+                print("\n-----------------------------LOGIN-----------------------------\n")
+                username = input("Enter Username: ").upper()
+                if username not in Users:
+                    print("Please enter valid username!")
+                    print(f"Valid Users: {Users}")
+                else:
+                    Userno = Users.index(username)
+                    password = input("Enter passsword [Case Sensitive]: ")
+                    if password != Passw[Userno]:
+                        print("Invalid Password! Try Again!")
+                    else:
+                        print("Menu Call")
+                        break         
+            if chc == 2:
+                while True:
+                    print("\n-----------------------------CHANGE PASSWORD-----------------------------\n")
+                    username = input("Enter Username: ").upper()
+                    if username not in Users:
+                        print("Please enter valid username!")
+                        print(f"Valid Users: {Users}")
+                        break
+                    else:
+                        Userno = Users.index(username)
+                        password = input("Enter passsword [Case Sensitive]: ")
+                        if password != Passw[Userno]:
+                            print("Invalid Password! Try Again!")
+                        else:
+                            new_password = ""
+                            for i in range(15):
+                                new_password = new_password + chr(random.randrange(42,122))
+                            print(new_password)
+                            Cursor.execute(f"UPDATE LOGIN SET PASSW = '{new_password}' where User = '{Users[Userno]}';")
+                            print("Password Updated!")
+                            break
+            if chc == 3:
+                break
         DataBase.commit()
-
 main()
 
