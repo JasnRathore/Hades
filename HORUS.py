e6d21d1b8e014780a5ad4e8aaf4b05fc24f1b354
tishagangar <146367529+tishagangar@users.noreply.github.com>
2023-09-29 23:37:56 +0530

    Add files via upload

--- HORUS.py
+++ HORUS.py
@@ -1,22 +1,97 @@
 from datetime import datetime,time
-import random
 from prettytable import PrettyTable
 from Initialisation import *
-
-def chk_time(time):
-    time_format="%H:%M:%S"
-    #Checking if format matches the time
+        
+def TimeValidate(Time): #Only valid for(HH:MM:SS,HHMMSS)
     try:
-        datetime.strptime(time,time_format)
+        if len(Time)== 8 and Time[2] in ':':
+            return 0 <= int(Time[0:2]) <=24 and 0 <= int(Time[3:5]) <= 59 and 0 <= int(Time[6:]) <= 59
+        elif len(Time)==6:
+            return 0 <= int(Time[0:2]) <=24 and 0 <= int(Time[2:4]) <= 59 and 0 <= int(Time[4:]) <= 59
     except ValueError:
-        return False
-    
-def str_to_time(time_str):
-    #Converting string to time
-    time_format="%H:%M:%S"
-    time=datetime.strptime(time_str,time_format).time()
-    return time
-    
+        pass
+    return False
+
+def InputGuards(Cursor,mode):
+    if mode=='A':
+        gno=random_no(Cursor,"guards")
+    elif mode=='M':
+        #Checking if gno exists
+        Cursor.execute(f"SELECT GNO FROM guards;")
+        guardnos=[]
+        for guard in Cursor.fetchall():
+            guardnos.append(guard[0])
+        while True:
+            gno = int(input("Enter GNO to Modify:"))
+            if gno in guardnos:
+                break
+            print ("GNO dosent exist\n")
+    Cursor.execute(f"SELECT NAME FROM guards;")
+    Names = []    
+    Name_lst=Cursor.fetchall()   
+    for rec in Name_lst:
+        Names.append(rec[0])
+    while True:
+        Name = input("Please enter name: ")
+        #Checking for valid name
+        if Name not in Names:
+            break
+        print("Records with inputted name exist!")
+        check_chc = (input("Do you wish to continue [Y/N]: ")).upper()
+        print()
+        if check_chc == "Y":
+            break
+    while True:
+        age=input("Please enter an age: ")
+        #Checking for valid age
+        if age.isdigit() and int(age)>18:
+            age=int(age)
+            break
+        elif mode=='M' and age == "":
+            break
+        else:
+            print("Please enter a valid age!")
+    while True:
+        gender = input("Please enter gender [M/F]: ").upper()
+        #Checking for valid gender
+        if (gender == "F" or gender == "M"):
+            break
+        elif mode=='M'and gender == "":
+            break
+        else:
+            print("Please select  valid gender!")      
+    while True:
+        salary = input("Please enter salary: ")
+        #Checking for valid salary
+        if salary.isdigit():
+            salary=int(salary)
+            break
+        elif mode=='M' and salary == "":
+            break
+        else:
+            print("Please enter a valid salary!")
+    while True:
+        cb = input("Please enter cell block [A/B/C]: ").upper()
+        #Checking for valid cellblock
+        if cb in ("A", "B", "C"):
+            break
+        elif mode=='M' and cb=="":
+            break
+        else:
+            print("Please select valid cell block!\n")
+    while True:
+        dst=input("Please enter duty start time (HH:MM:SS,HHMMSS):")
+        if TimeValidate(dst) is True or (mode == "M" and dst == ""):
+            break
+        print("\nInvalid time\n")  
+    while True:
+        det=input("Please enter duty end time (HH:MM:SS,HHMMSS):")
+        if TimeValidate(det) is True or (mode == "M" and det == ""):
+            break
+        print("\nInvalid time\n")   
+
+    return gno,Name,age,gender,salary,cb,dst,det
+
 def view(Cursor):
     Cursor.execute(f"SELECT * FROM guards;")
     mytable = PrettyTable(["GNO","Name","Age","Gen","Salary","Cell","StartTime","EndTime"])
@@ -25,152 +100,50 @@ def view(Cursor):
         for data in row:
             lst.append(str(data))
         mytable.add_row(lst)
-
     print(mytable)
-        
-def add(Cursor):
+
+def add(DataBase,Cursor):
     while True:
         print("--------------------------------Add Record--------------------------------")
-        gno=random_no(Cursor,"guards")
-        name = input("Please enter name: ")
-        #Checking for name repetition
-        
-        Cursor.execute(f"SELECT * FROM guards;")
-        result = Cursor.fetchall()
-        for i in result:
-            if i[1] == name:
-                print("Records with inputted name exist!")
-                check_chc = (input("Do you wish to continue [Y/N]: ")).upper()
-                print()
-                if check_chc == "Y":
-                    pass
-                else:
-                    break
-            
-        age = int(input("Please enter age: "))
-
-        gender = input("Please enter gender [M/F]: ").upper()
-        #Checking for valid gender
-        if gender != "F" and gender != "M":
-            print("Please select  valid gender!\n")
-            break
-
-        salary=int(input("Please enter salary: "))
-        
-        cb = input("Please enter cell block [A/B/C]: ").upper()
-        #Checking for valid cellblock
-        if cb not in ("A", "B", "C"):
-            print("Please select  valid cell block!\n")
-            break
-            
-        dst=input("Please enter duty start time[hh:mm:ss]: ")
-        if chk_time(dst)==False:
-            print("Invalid time string ")
-            break
-
-        det=input("Please enter duty end time[hh:mm:ss]: ")
-        if chk_time(det)==False:
-            print("Invalid time string ")
-            break
-            
-        data=[gno,name,age,gender,salary,cb,dst,det]
-        insert=(f"INSERT INTO guards values (%s,%s,%s,%s,%s,%s,%s,%s)")
-        Cursor.execute(insert,data)
+        fields=InputGuards(Cursor,'A')
+        Cursor.execute("INSERT INTO guards VALUES(%s, %s, %s, %s, %s, %s, %s, %s);",fields)
+        DataBase.commit()
         print("\nRecord added\n")
         add_chc = input("Do you wish to add another record? [Y/N]: ").upper()
         if add_chc == "Y":
             continue
         else:
             break
-  
-def modify(Cursor):
+
+def modify(DataBase,Cursor):
     while True:
         view(Cursor)
         print("--------------------------------Modify Record--------------------------------")
-        gno=int(input("Enter the guard number of the record to be modified: "))
-        #Checking if gno exists
-        Cursor.execute("SELECT * from guards")
-        result = Cursor.fetchall()
-        for i in result:
-            if i[0] == gno:
-                guard_info = list(i)
-                del guard_info[0]
-                break
-        else:
-            print("Gno not found!")
-            break
-        print("Press Enter if you do not wish to modify field!\n")
-        name = input("Please enter name: ")
-        age = input("Please enter an age: ")
-        if age.isdigit() or age == "":
-            pass
-        else:
-            print("Please enter a valid age!")
-            break
-
-        gender = input("Please enter gender [M/F]: ").upper()
-
-        #Checking for Valid Gender
-        if (gender == "F" or gender == "M"):
-            pass
-        elif gender == "":
-            pass
-        else:
-            print("Please select  valid gender!")
-            break
-            
-        salary=input("Please enter salary: ")
-        if age.isdigit() or age == "":
-            pass
-        else:
-            print("Please enter a valid salary!")
-            break
-        cb = input("Please enter cell block [A/B/C]: ").upper()
-        
-        #Checking for valid cellblock
-        if cb not in ("A","B","C","") :
-            print("Please select  valid cell block!\n")
-            break
-            
-        dst_str=input("Please enter duty start time[hh:mm:ss]: ")
-        if chk_time(dst_str)==True:
-            dst_str=str_to_time(dst_str)
-        elif dst_str == "":
-            pass
-        else:
-            print("Invalid time string!")
-            break
-
-        det_str=input("Please enter duty end time[hh:mm:ss]: ")
-        if chk_time(det_str)==True:
-            det_str=str_to_time(det_str)
-        elif det_str == "":
-            pass
-        else:
-            print("Invalid time string!")
-
-        data=[name,age,gender,salary,cb,dst_str,det_str,gno]
-        for i in range(len(data)):
-            if data[i] == "":
-                data[i] = guard_info[i]
-
+        fields=list(InputGuards(Cursor,'M'))
+        Cursor.execute(f"SELECT * FROM guards WHERE GNO={fields[0]};")
+        PreviousData=Cursor.fetchone()
+        for i in range(len(fields)):
+            if fields[i]=="":
+                fields[i]=PreviousData[i]
+        gno=fields.pop(0)   
+        fields.append(gno)   
         update = f"UPDATE guards SET NAME = %s,AGE = %s,GENDER = %s,SALARY = %s,CELL_BLOCK = %s,DUTY_START_TIME = %s,DUTY_END_TIME = %s WHERE gno = %s;"
-        Cursor.execute(update,data)
-        print("\nRecord updated\n")
-        
+        Cursor.execute(update,fields)
+        DataBase.commit()
+        print("\nRecord updated\n")    
         modify_chc = input("Do you wish to modify another record? [Y/N]: ").upper()
         if modify_chc == "Y":
             continue
         else:
             break
-
-def delete(Cursor):
+        
+def delete(DataBase,Cursor):
     while True: 
         view(Cursor)
         print("--------------------------------Delete Record--------------------------------")
         gno=int(input("Enter the guard number of the record to be deleted: "))
-         #Checking if gno exists
-        Cursor.execute("SELECT * from guards")
+        #Checking if gno exists
+        Cursor.execute(f"SELECT * from guards")
         result = Cursor.fetchall()
         for i in result:
             if int(i[0]) == gno:
@@ -180,6 +153,7 @@ def delete(Cursor):
             break
         delete=f"DELETE FROM guards WHERE GNO={gno}"
         Cursor.execute(delete)
+        DataBase.commit()
         print("\nRecord deleted\n")
         del_chc = input("Do you wish to delete another record? [Y/N]: ").upper()
         if del_chc == "Y":
@@ -187,7 +161,7 @@ def delete(Cursor):
         else:
             break
 
-def menu(Cursor,Guards):
+def guards_menu(DataBase,Cursor):
     while True:
         print('''
 --------------------------------Menu--------------------------------
@@ -195,22 +169,21 @@ def menu(Cursor,Guards):
 2) Add Guard Details
 3) Modify Guard Details
 4) Delete Guard Details
-5) Exit
-''')
+5) Exit''')
         menu_chc = int(input("Please select a choice [1/2/3/4/5]: "))
-        database = Guards
         if menu_chc==1:
             view(Cursor)
         elif menu_chc==2:
-            add(Cursor)
+            add(DataBase,Cursor)
         elif menu_chc==3:
-            modify(Cursor)
+            modify(DataBase,Cursor)
         elif menu_chc==4:
-            delete(Cursor)
+            delete(DataBase,Cursor)
         elif menu_chc==5:
             break
         else:
             print("Invalid input! ")
-            break
+           
     
     
+
