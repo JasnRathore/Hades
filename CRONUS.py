f37a3d307ef2cf21d40f35ee902a2cfbff97a364
JasnRathore <139693551+JasnRathore@users.noreply.github.com>
2023-10-02 21:45:05 +0530

    Update CRONUS.py

--- CRONUS.py
+++ CRONUS.py
@@ -1,166 +1,185 @@
 from prettytable import PrettyTable
 from Initialisation import *
 
-def DateValidate(Date): #Only Valid for(YYYY-MM-DD, YYYY/MM/DD, YYYYMMDD)
+def TimeValidate(Time): #Only valid for(HH:MM:SS,HHMMSS)
     try:
-        if len(Date) == 10 and Date[4] in '-/':
-            return 1 <= int(Date[5:7]) <= 12 and 1 <= int(Date[8:]) <= 31
-        elif len(Date) == 8:
-            return 1 <= int(Date[4:6]) <= 12 and 1 <= int(Date[6:]) <= 31
+        if len(Time)== 8 and Time[2] in ':':
+            return 0 <= int(Time[0:2]) <=24 and 0 <= int(Time[3:5]) <= 59 and 0 <= int(Time[6:]) <= 59
+        elif len(Time)==6:
+            return 0 <= int(Time[0:2]) <=24 and 0 <= int(Time[2:4]) <= 59 and 0 <= int(Time[4:]) <= 59
     except ValueError:
         pass
     return False
 
-def GetValidInput(Prompt, ValidOptions):
-    while True:
-        UserInput = input(Prompt).upper()
-        if UserInput in ValidOptions:
-            return UserInput
-        print("Invalid INPUT\n")
-
-def InputPrisonerData(Cursor, Mode = "A"):
-    if Mode == 'A':
-        PNO = random_no(Cursor, "prisoners")
-    elif Mode == 'M':
-        Cursor.execute(f"SELECT PNO FROM prisoners;")
-        ListOfPrisonersNos = [prisoner[0] for prisoner in Cursor.fetchall()]
+def InputGuards(Cursor,mode):
+    if mode=='A':
+        GNO = random_no(Cursor,"guards")
+    elif mode=='M':
+        #Checking if GNO exists
+        Cursor.execute(f"SELECT GNO FROM guards;")
+        ListOfGuardNos=[]
+        for Guard in Cursor.fetchall():
+            ListOfGuardNos.append(Guard[0])
         while True:
-            PNO = int(input("Enter PNO to Modify:"))
-            if PNO in ListOfPrisonersNos:
+            GNO = int(input("Enter GNO to Modify:"))
+            if GNO in ListOfGuardNos:
                 break
-            print ("PNO dosent exist\n")
-
-    Cursor.execute(f"SELECT NAME FROM prisoners;")
+            print ("GNO dosent exist\n")
+    Cursor.execute(f"SELECT NAME FROM guards;")
     ListOfNames = []
     for Row in Cursor.fetchall():
-        ListOfNames.append(Name[Row])
+        ListOfNames.append(Row[0])
     while True:
         Name = input("Please enter name: ")
-        if (Name == "" and Mode == "A"):
+        if (Name == "" and mode == "A"):
             print("Cant leave Input field empty")
-        elif (Name == "" and Mode == "M") or (Name not in ListOfNames):
+        elif (Name == "" and mode == "M") or (Name not in ListOfNames):
             break
         else:
             print("Records with inputted name exist!")
-            if GetValidInput("Do you wish to continue [Y/N]: ",("Y", "N")) == "Y":
+            check_chc = (input("Do you wish to continue [Y/N]: ")).upper()
+            print()
+            if check_chc == "Y":
                 break
+    while True:
+        age=input("Please enter an age: ")
+        #Checking for valid age
+        if age.isdigit() and int(age)>18:
+            age=int(age)
+            break
+        elif mode=='M' and age == "":
+            break
+        else:
+            print("Please enter a valid age!")
+    while True:
+        gender = input("Please enter gender [M/F]: ").upper()
+        #Checking for valid gender
+        if (gender == "F" or gender == "M"):
+            break
+        elif mode=='M'and gender == "":
+            break
+        else:
+            print("Please select  valid gender!")
+    while True:
+        salary = input("Please enter salary: ")
+        #Checking for valid salary
+        if salary.isdigit():
+            salary=int(salary)
+            break
+        elif mode=='M' and salary == "":
+            break
+        else:
+            print("Please enter a valid salary!")
+    while True:
+        cb = input("Please enter cell block [A/B/C]: ").upper()
+        #Checking for valid cellblock
+        if cb in ("A", "B", "C"):
+            break
+        elif mode=='M' and cb=="":
+            break
+        else:
+            print("Please select valid cell block!\n")
+    while True:
+        dst=input("Please enter duty start time (HH:MM:SS,HHMMSS):")
+        if TimeValidate(dst) is True or (mode == "M" and dst == ""):
+            break
+        print("\nInvalid time\n")
+    while True:
+        det=input("Please enter duty end time (HH:MM:SS,HHMMSS):")
+        if TimeValidate(det) is True or (mode == "M" and det == ""):
+            break
+        print("\nInvalid time\n")
 
-    while True:
-        Age = input("Please enter age: ")
-        if Age.isdigit() or (Mode == 'M' and Age ==""):
-            if Age.isdigit():
-                Age = int(Age)
-            break
-        print("invalid Age\n")
+    return GNO,Name,age,gender,salary,cb,dst,det
 
-    while True:
-        Gender = input("Please enter gender [M/F]: ").upper()
-        if Gender in ("M", "F") or (Mode == "M" and Gender == ""):
-            break
-        print("Invalid gender. Please enter 'M' or 'F'.")
+def view(Cursor):
+    Cursor.execute(f"SELECT * FROM guards;")
+    mytable = PrettyTable(["GNO","Name","Age","Gen","Salary","Cell","StartTime","EndTime"])
+    for row in Cursor:
+        lst=[]
+        for data in row:
+            lst.append(str(data))
+        mytable.add_row(lst)
+    print(mytable)
 
+def add(DataBase,Cursor):
     while True:
-        Crime = input("Enter Crime: ")
-        if Crime != "" or (Crime == "" and Mode == "M"):
-            break
-        print("Invalid Input\n")
-
-    while True:
-        Sentence = input("Please enter Sentence: ")
-        if Sentence.isdigit() or (Mode == 'M' and Sentence ==""):
-            if Sentence.isdigit():
-                Sentence = int(Sentence)
-            break
-        print("invalid Sentence\n")
-
-    while True:
-        CellBlock = input("Please enter cell block [A/B/C]: ").upper()
-        if CellBlock in ("A", "B", "C")  or (Mode == "M" and CellBlock ==""):
-            break
-        print("Invalid cell block. Please enter 'A', 'B', or 'C'.")
-
-    while True:
-        Parole = input("Allow Parole [Y/N]:").upper()
-        if  Parole == "Y":
-            Parole = "YES"
-            break
-        elif Parole == "N":
-            Parole = "NO"
-            break
-        elif Mode == "M" and Parole == "":
-            break
-        print("\ninvalid Option\n")
-    while True:
-        ReleaseDate = input("Enter Release Date(YYYY-MM-DD, YYYY/MM/DD, YYYYMMDD):")
-        if DateValidate(ReleaseDate) is True or (Mode == "M" and ReleaseDate == ""):
-            break
-        print("\nInvalid Date\n")
-    return PNO, Name, Age, Gender, Crime, Sentence, CellBlock, Parole, ReleaseDate
-
-def AddPrisoner(DataBase, Cursor):
-    while True:
-        print("Add record")
-        PrisonerData = InputPrisonerData(Cursor, "A")
-        Cursor.execute("INSERT INTO prisoners VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s);", PrisonerData)
+        print("--------------------------------Add Record--------------------------------")
+        fields=InputGuards(Cursor,'A')
+        Cursor.execute("INSERT INTO guards VALUES(%s, %s, %s, %s, %s, %s, %s, %s);",fields)
         DataBase.commit()
         print("\nRecord added\n")
-        if GetValidInput("Do you wish to add another record? [Y/N]: ", ("Y", "N")) == "N":
+        add_chc = input("Do you wish to add another record? [Y/N]: ").upper()
+        if add_chc == "Y":
+            continue
+        else:
             break
 
-def ModifyPrisoner(DataBase, Cursor):
+def modify(DataBase,Cursor):
     while True:
-        print("Modify record")
-        PrisonerData = list(InputPrisonerData(Cursor, "M"))
-        Cursor.execute(f"SELECT * FROM Prisoners WHERE PNO = {PrisonerData[0]};")
-        PreviousPrisonerData = Cursor.fetchone()
-        for i in range(len(PrisonerData)):
-            if PrisonerData[i] == "":
-                PrisonerData[i] = PreviousPrisonerData[i]
-        PNO = PrisonerData.pop(0)
-        PrisonerData.append(PNO)
-        Cursor.execute("UPDATE prisoners SET NAME = %s, AGE = %s, GENDER = %s, CRIME = %s, SENTENCE = %s, CELL_BLOCK = %s, PAROLE = %s, RELEASE_DATE = %s WHERE PNO = %s;", tuple(PrisonerData))
+        view(Cursor)
+        print("--------------------------------Modify Record--------------------------------")
+        fields=list(InputGuards(Cursor,'M'))
+        Cursor.execute(f"SELECT * FROM guards WHERE GNO={fields[0]};")
+        PreviousData=Cursor.fetchone()
+        for i in range(len(fields)):
+            if fields[i]=="":
+                fields[i]=PreviousData[i]
+        GNO=fields.pop(0)
+        fields.append(GNO)
+        update = f"UPDATE guards SET NAME = %s,AGE = %s,GENDER = %s,SALARY = %s,CELL_BLOCK = %s,DUTY_START_TIME = %s,DUTY_END_TIME = %s WHERE GNO = %s;"
+        Cursor.execute(update,fields)
         DataBase.commit()
-        print("\nRecord Updated\n")
-        if GetValidInput("Do you wish to Update another record? [Y/N]: ", ("Y", "N")) == "N":
+        print("\nRecord updated\n")
+        modify_chc = input("Do you wish to modify another record? [Y/N]: ").upper()
+        if modify_chc == "Y":
+            continue
+        else:
             break
 
-def DeletePrisoner(DataBase, Cursor):
+def delete(DataBase,Cursor):
     while True:
-        Cursor.execute("SELECT PNO FROM prisoners;")
-        ListOfPrisonersNos = [prisoner[0] for prisoner in Cursor.fetchall()]
-        PNO = int(input("Enter PNO to DELETE:"))
-        if PNO not in ListOfPrisonersNos:
-            print ("PNO dosent exist\n")
+        view(Cursor)
+        print("--------------------------------Delete Record--------------------------------")
+        GNO=int(input("Enter the guard number of the record to be deleted: "))
+        #Checking if GNO exists
+        Cursor.execute(f"SELECT * from guards")
+        result = Cursor.fetchall()
+        for i in result:
+            if int(i[0]) == GNO:
+                break
+        else:
+            print("Gno not found!")
             break
-        Cursor.execute(f"DELETE FROM prisoners WHERE PNO = {PNO}")
+        delete=f"DELETE FROM guards WHERE GNO={GNO}"
+        Cursor.execute(delete)
         DataBase.commit()
-        print("\nRecord Deleted\n")
-        if GetValidInput("Do you wish to Delete another record? [Y/N]: ", ("Y", "N")) == "N":
+        print("\nRecord deleted\n")
+        del_chc = input("Do you wish to delete another record? [Y/N]: ").upper()
+        if del_chc == "Y":
+            continue
+        else:
             break
 
-def PrisonerView(Cursor):
-    Cursor.execute("SELECT * FROM prisoners;")
-    MyTable = PrettyTable(["PNO","Name","Age","Gen","Crime", "Sentence","Cell","Parole","RD"])
-    for row in Cursor.fetchall():
-        MyTable.add_row(list(row))
-    print(MyTable)
-
-def PrisonerMenu(DataBase, Cursor):
-    Options = {1: PrisonerView, 2: AddPrisoner, 3: ModifyPrisoner, 4: DeletePrisoner}
+def guards_menu(DataBase,Cursor):
     while True:
         print('''
-1) View Prisoner Details
-2) Add Prisoner Details
-3) Modify Prisoner Details
-4) Delete Prisoner Details
-5) Exit
-''')
-        SelectedMenuOption = int(input("Please select a choice [1/2/3/4/5]: "))
-        if SelectedMenuOption == 1:
-            Options[SelectedMenuOption](Cursor)
-        elif SelectedMenuOption == 5:
+--------------------------------Menu--------------------------------
+1) View Guard Details
+2) Add Guard Details
+3) Modify Guard Details
+4) Delete Guard Details
+5) Exit''')
+        menu_chc = int(input("Please select a choice [1/2/3/4/5]: "))
+        if menu_chc==1:
+            view(Cursor)
+        elif menu_chc==2:
+            add(DataBase,Cursor)
+        elif menu_chc==3:
+            modify(DataBase,Cursor)
+        elif menu_chc==4:
+            delete(DataBase,Cursor)
+        elif menu_chc==5:
             break
-        elif SelectedMenuOption in Options:
-            Options[SelectedMenuOption](DataBase, Cursor)
         else:
             print("Invalid input! ")
